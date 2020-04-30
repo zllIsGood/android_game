67ab99288ba271bcf3336cee23dfd7a98448a46d
zhoulanglang <1595189825@qq.com>
2020-04-30 16:44:48 +0800

    debug-log关闭

--- app/src/main/java/com/egret/openadsdk/sdk/TTAdManagerHolder.java
+++ app/src/main/java/com/egret/openadsdk/sdk/TTAdManagerHolder.java
@@ -42,7 +42,7 @@ public class TTAdManagerHolder {
                 .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
                 .allowShowNotify(true) //是否允许sdk展示通知栏提示
                 .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
-                .debug(true) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
+                .debug(false) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
                 .directDownloadNetworkType() //允许直接下载的网络状态集合
                 .supportMultiProcess(true)//是否支持多进程
                 .needClearTaskReset()
